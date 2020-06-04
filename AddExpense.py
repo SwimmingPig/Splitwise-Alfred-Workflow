f7ee5ed21df69e081f763f04d1bf0d663c7125bc
Andy Chu <aandychutwa@gmail.com>
2020-06-03 20:12:06 -0700

    Finish Core Code and Use Pyinstaller to package python code

--- AddExpense.py
+++ AddExpense.py
@@ -1,10 +1,27 @@
+import sys
 from splitwise import Splitwise
 from splitwise.expense import Expense
 from splitwise.user import ExpenseUser, Friend
 
+
+def getUserInput():
+    Name = sys.argv[1]
+    TargetUser = sys.argv[2]
+    Amount = float(sys.argv[3])
+    Ratio = 0.5
+
+    if len(sys.argv) > 4:
+        Ratio = float(sys.argv[4])
+
+    My_Amount = str(round(Amount * Ratio, 2))
+    Target_Amount = str(Amount - float(My_Amount))
+    return [Name, TargetUser, Amount, My_Amount, Target_Amount]
+
+Name, TargetUser, Amount, My_Amount, Target_Amount = getUserInput()
+
 Consumer_Key = 'Your Consumer Key'
 Consumer_Secret = 'Your Secret Key'
-access_token = 'Your Access Token'
+access_token = 'Get Your Access Token From GetAccessToken.py'
 
 sObj = Splitwise(Consumer_Key,Consumer_Secret)
 sObj.setAccessToken(access_token)
@@ -15,18 +32,18 @@ for friend in sObj.getFriends():
     friends[friend.first_name] = friend
 
 expense = Expense()
-expense.setCost('10')
-expense.setDescription("Testing")
+expense.setCost(str(Amount))
+expense.setDescription(Name)
 
 user1 = ExpenseUser()
 user1.setId(user.id)
-user1.setPaidShare('10.00')
-user1.setOwedShare('5.00')
+user1.setPaidShare(str(Amount))
+user1.setOwedShare(My_Amount)
 
 user2 = ExpenseUser()
-user2.setId(friends['name'].id)
+user2.setId(friends[TargetUser].id)
 user2.setPaidShare('0')
-user2.setOwedShare('5.0')
+user2.setOwedShare(Target_Amount)
 
 users = []
 users.append(user1)
--- GetAccessToken.py
+++ GetAccessToken.py
@@ -5,7 +5,8 @@ Consumer_Secret = 'Your Secret Key'
 
 sObj = Splitwise(Consumer_Key,Consumer_Secret)
 token, secret = sObj.getAuthorizeURL()
-oauth_verifier = 'Get from your http response'
+# Go to ""'https://secure.splitwise.com/authorize?oauth_token?' + token" to get your oauth_verifier
+# Then uncomment the code below to get your access token, and place the token in AddExpense.py
 
-access_token = sObj.getAccessToken(token, secret, oauth_verifier)
-print(access_token)
+# access_token = sObj.getAccessToken(token, secret, oauth_verifier)
+# print(access_token)
